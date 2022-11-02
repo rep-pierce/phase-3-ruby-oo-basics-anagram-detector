243beb5fd8d9b58ba535bb8abf1635f0f8264d25
Ryan Pierce <pierce.ryan.97@gmail.com>
2022-11-01 22:04:38 -0600

    commit tree 20e24902a651f85fea5338c73e5b5049a9951922

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+attr_accessor :word, :anagrams
+    def initialize (word)
+        @word = word
+    end
+
+    def match (anagrams)
+        anagrams.filter { |anagram| anagram.chars.sort == @word.chars.sort}
+    end
+end
